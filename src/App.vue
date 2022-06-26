aea681497ac36b6d65ddc2afc4b64af4fbdfdcbd
nanagami1369 <nanagami1369@gmail.com>
2022-06-26 17:12:07 +0900

    カレンダのヘッダーを作成

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,34 @@
 <script setup lang="ts">
+const createCalendarDate = (year: number, month: number): CalendarCell[] => {
+  const calendar = new Array<CalendarCell>(42);
+  let calendarIndex = 0;
+  const prevLastDate = new Date(year, month, 1);
+  prevLastDate.setDate(0);
+  const prevLastDayOfWeek = prevLastDate.getDay();
+  // 先月
+  for (let index = prevLastDayOfWeek; index >= 0; index--) {
+    const day = prevLastDate.getDate() - index;
+    calendar[calendarIndex] = new CalendarCell(day, false);
+    calendarIndex++;
+  }
+
+  // 今月
+  const currentLastDate = new Date(year, month, 1);
+  currentLastDate.setMonth(currentLastDate.getMonth() + 1);
+  currentLastDate.setDate(0);
+  for (let day = 1; day <= currentLastDate.getDate(); day++) {
+    calendar[calendarIndex] = new CalendarCell(day, true);
+    calendarIndex++;
+  }
+  // 来月
+  const remainingDate = calendar.length - calendarIndex;
+  for (let day = 1; day <= remainingDate; day++) {
+    calendar[calendarIndex] = new CalendarCell(day, false);
+    calendarIndex++;
+  }
+  return calendar;
+};
+
 class CalendarCell {
   day: Readonly<number>;
   isCurrent: Readonly<boolean>;
@@ -8,43 +38,55 @@ class CalendarCell {
     this.isCurrent = isCurrent;
   }
 }
-
-const calendar = new Array<CalendarCell>(42);
-let calendarIndex = 0;
-const prevLastDate = new Date();
-prevLastDate.setDate(0);
-const prevLastDayOfWeek = prevLastDate.getDay();
-// 先月
-for (let index = prevLastDayOfWeek; index >= 0; index--) {
-  const day = prevLastDate.getDate() - index;
-  calendar[calendarIndex] = new CalendarCell(day, false);
-  calendarIndex++;
+// url queryから表示する年月の情報を取得
+let query: string;
+if (location.search.startsWith("?date=")) {
+  query = location.search.substring("?date=".length).split("&")[0];
+} else {
+  query = "";
 }
-
-// 今月
-const currentLastDate = new Date();
-currentLastDate.setMonth(currentLastDate.getMonth() + 1);
-currentLastDate.setDate(0);
-for (let day = 1; day <= currentLastDate.getDate(); day++) {
-  calendar[calendarIndex] = new CalendarCell(day, true);
-  calendarIndex++;
+// 表示するカレンダーの年と月
+let current: Date;
+if (/^[0-9]{4}-[0-9]{1,2}$/.test(query)) {
+  const YearAndMonthPair = query.split("-");
+  const year = Number(YearAndMonthPair[0]);
+  const month = Number(YearAndMonthPair[1]);
+  // 正規表現で数字あることを判定済みなため、文字列が数字以外の可能性を考慮しない
+  // month(月)は0始まりなので - 1
+  current = new Date(year, month - 1);
+} else {
+  current = new Date();
 }
-// 来月
-const remainingDate = calendar.length - calendarIndex;
-for (let day = 1; day <= remainingDate; day++) {
-  calendar[calendarIndex] = new CalendarCell(day, false);
-  calendarIndex++;
-}
-console.log(calendar);
+const calendar = createCalendarDate(current.getFullYear(), current.getMonth());
+const prevMonth = (): void => {
+  const prevMonth = new Date(current.getFullYear(), current.getMonth());
+  prevMonth.setMonth(prevMonth.getMonth() - 1);
+  location.search = `?date=${prevMonth.getFullYear()}-${
+    prevMonth.getMonth() + 1
+  }`;
+};
+const nextMonth = (): void => {
+  const nextMonth = new Date(current.getFullYear(), current.getMonth());
+  nextMonth.setMonth(nextMonth.getMonth() + 1);
+  location.search = `?date=${nextMonth.getFullYear()}-${
+    nextMonth.getMonth() + 1
+  }`;
+};
+const nowMonth = (): void => {
+  const now = new Date();
+  location.search = `?date=${now.getFullYear()}-${now.getMonth() + 1}`;
+};
 </script>
 
 <template>
   <main>
     <header class="calendar-header">
-      <button class="arrow-button">&lt;</button>
-      <section class="current-date">2022/06</section>
-      <button class="arrow-button">&gt;</button>
-      <button>現在</button>
+      <button @click="prevMonth" class="arrow-button">&lt;</button>
+      <section class="current-date">
+        {{ current.getFullYear() }}/{{ current.getMonth() + 1 }}
+      </section>
+      <button @click="nextMonth" class="arrow-button">&gt;</button>
+      <button @click="nowMonth">現在</button>
     </header>
     <table>
       <thead class="day-of-week">
