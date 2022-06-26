e868ed8715fb5fd8b9772a512c5d992fb0e95595
nanagami1369 <nanagami1369@gmail.com>
2022-06-26 14:57:59 +0900

    カレンダを作成した

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
-const calendar = new Array<number>(42);
+class CalendarCell {
+  day: Readonly<number>;
+  isCurrent: Readonly<boolean>;
+
+  constructor(day: number, isCurrent: boolean) {
+    this.day = day;
+    this.isCurrent = isCurrent;
+  }
+}
+
+const calendar = new Array<CalendarCell>(42);
 let calendarIndex = 0;
 const prevLastDate = new Date();
 prevLastDate.setDate(0);
 const prevLastDayOfWeek = prevLastDate.getDay();
 // 先月
 for (let index = prevLastDayOfWeek; index >= 0; index--) {
-  calendar[calendarIndex] = prevLastDate.getDate() - index;
+  const day = prevLastDate.getDate() - index;
+  calendar[calendarIndex] = new CalendarCell(day, false);
   calendarIndex++;
 }
 
@@ -14,14 +25,14 @@ for (let index = prevLastDayOfWeek; index >= 0; index--) {
 const currentLastDate = new Date();
 currentLastDate.setMonth(currentLastDate.getMonth() + 1);
 currentLastDate.setDate(0);
-for (let index = 1; index <= currentLastDate.getDate(); index++) {
-  calendar[calendarIndex] = index;
+for (let day = 1; day <= currentLastDate.getDate(); day++) {
+  calendar[calendarIndex] = new CalendarCell(day, true);
   calendarIndex++;
 }
 // 来月
 const remainingDate = calendar.length - calendarIndex;
-for (let index = 1; index <= remainingDate; index++) {
-  calendar[calendarIndex] = index;
+for (let day = 1; day <= remainingDate; day++) {
+  calendar[calendarIndex] = new CalendarCell(day, false);
   calendarIndex++;
 }
 console.log(calendar);
@@ -32,19 +43,28 @@ console.log(calendar);
     <table>
       <thead class="day-of-week">
         <tr>
-          <td>日</td>
+          <td class="sunday">日</td>
           <td>月</td>
           <td>火</td>
           <td>水</td>
           <td>木</td>
           <td>金</td>
-          <td>土</td>
+          <td class="saturday">土</td>
         </tr>
       </thead>
       <tbody>
         <tr v-for="y in 6" :key="y">
-          <td v-for="x in 7" :key="x" class="calendar-cell">
-            {{ calendar[x + 7 * y - 8] }}
+          <td
+            v-for="x in 7"
+            :key="x"
+            class="calendar-cell"
+            :class="{
+              'is-current-day': calendar[x + 7 * y - 8].isCurrent,
+              saturday: x == 7,
+              sunday: x == 1,
+            }"
+          >
+            <span>{{ calendar[x + 7 * y - 8].day }}</span>
           </td>
         </tr>
       </tbody>
@@ -55,24 +75,37 @@ console.log(calendar);
 <style lang="scss">
 main {
   height: 100%;
+  color: #191920;
   table {
     width: 100%;
     height: 100%;
     padding: 0px;
     border-collapse: collapse;
-  }
-
-  td {
-    text-align: center;
-    vertical-align: top;
-    font-weight: bold;
-    font-size: 2em;
-    border: solid 2px #535360;
-  }
-
-  thead.day-of-week {
-    tr {
-      height: 20px;
+    td {
+      text-align: center;
+      vertical-align: top;
+      font-weight: bold;
+      font-size: 2em;
+      border: solid 2px #535360;
+      background-color: #f2f2f6;
+    }
+    td.sunday {
+      color: #cc1b1b;
+    }
+    td.saturday {
+      color: #351bcc;
+    }
+    thead.day-of-week {
+      tr {
+        height: 20px;
+      }
+    }
+    tbody {
+      td:not(.is-current-day) {
+        span {
+          opacity: 0.3;
+        }
+      }
     }
   }
 }
